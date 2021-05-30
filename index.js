b492117dbf218be1db8061a09a748fa4d6c23057
hilelley <67314778+hilelley@users.noreply.github.com>
2021-05-30 11:19:37 +0300

    .

--- index.js
+++ index.js
@@ -24,6 +24,16 @@ if ("serviceWorker" in navigator) {
 let deferredPrompt;
 const addBtn = document.querySelector(".add-button");
 // addBtn.style.display = "none";
+deferredPrompt.prompt();
+// Wait for the user to respond to the prompt
+deferredPrompt.userChoice.then((choiceResult) => {
+  if (choiceResult.outcome === "accepted") {
+    console.log("User accepted the A2HS prompt");
+  } else {
+    console.log("User dismissed the A2HS prompt");
+  }
+  deferredPrompt = null;
+});
 
 window.addEventListener("beforeinstallprompt", (e) => {
   // Prevent Chrome 67 and earlier from automatically showing the prompt
